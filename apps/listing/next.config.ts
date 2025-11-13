f4bcb78507659ef820e61b4af0a37248822d9088
Dev Shakya <devxoshakya@gmail.com>
2025-11-13 19:04:06 +0530

    feat: update configuration files and add deployment workflow for Cloudflare Workers

--- apps/listing/next.config.ts
+++ apps/listing/next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	typedRoutes: true,
 	reactCompiler: true,
+	typescript : {
+		ignoreBuildErrors : true,
+	},
+	images : {
+		unoptimized : true,
+	}
 };
 
 export default nextConfig;
